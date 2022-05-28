0ecd0ec48ec11124230b564c74fb2bf1c9dbf62b
mo475cam <105428974+mo475cam@users.noreply.github.com>
2022-05-28 01:24:46 +0100

    Update parse.py

--- logsim/parse.py
+++ logsim/parse.py
@@ -41,4 +41,95 @@ class Parser:
         # For now just return True, so that userint and gui can run in the
         # skeleton code. When complete, should return False when there are
         # errors in the circuit definition file.
+
+        def devices_list(self):
+            if self.symbol.type == self.scanner.KEYWORD and self.symbol.id == self.scanner.DEVICES:
+                self.symbol = self.scanner.get_symbol()
+                if self.symbol.type == self.scanner.COLON:
+                    self.symbol = self.scanner.get_symbol()
+                    self.device()
+                    while self.symbol.type != self.scanner.SEMICOLON:
+                        self.symbol = self.scanner.get_symbol()
+                        self.device()
+                    if self.symbol.type == self.scanner.KEYWORD and self.symbol.id == self.scanner.ENDDEVICES:
+                        self.symbol = self.scanner.get_symbol()
+                    else:
+                        self.error()
+                else:
+                    self.error()
+            else:
+                self.error()
+
+        def connection_list():
+            if self.symbol.type == self.scanner.KEYWORD and self.symbol.id == self.scanner.CONNECTIONS:
+                self.symbol = self.scanner.get_symbol()
+                if self.symbol.type == self.scanner.COLON:
+                    self.symbol = self.scanner.get_symbol()
+                    self.connection()
+                    while self.symbol.type != self.scanner.KEYWORD:
+                        self.symbol = self.scanner.get_symbol()
+                        self.connection()
+                    if self.symbol.type == self.scanner.KEYWORD and self.symbol.id == self.scanner.ENDCONNECTIONS:
+                        self.symbol = self.scanner.get_symbol()
+                    else:
+                        self.error()
+                else:
+                    self.error()
+            else:
+                self.error()
+
+        def monitor(self):
+            if self.symbol.type == self.scanner.KEYWORD and self.symbol.id == self.scanner.MONITOR:
+                self.symbol = self.scanner.get_symbol()
+                self.output()
+                while (self.symbol.type == self.scanner.KEYWORD and self.symbol.id == self.scanner.AND) or \
+                        (self.symbol.type == self.scanner.COMMA):
+                    self.symbol = self.scanner.get_symbol()
+                    self.output()
+                if self.symbol.type == self.scanner.SEMICOLON:
+                    self.symbol = self.scanner.get_symbol()
+                else:
+                    self.error()
+
+        def device(self):
+            self.name()
+            self.symbol = self.scanner.get_symbol()
+            if self.symbol.type == self.scanner.KEYWORD and self.symbol.id == self.scanner.IS:
+                self.symbol = self.scanner.get_symbol()
+                self.gate()
+                if self.symbol.type == self.scanner.SEMICOLON:
+                    self.symbol = self.scanner.get_symbol()
+                else:
+                    self.error()
+            else:
+                self.error()
+
+        def gate(self):
+            if self.symbol.type == self.scanner.KEYWORD:
+                if self.symbol.id == self.scanner.SWITCH:
+                    self.switch()
+                elif self.symbol.id == self.scanner.CLOCK:
+                    self.clock()
+                elif self.symbol.id == self.scanner.AND_GATE:
+                    self.and_gate()
+                elif self.symbol.id == self.scanner.NAND:
+                    self.nand()
+                elif self.symbol.id == self.scanner.OR_GATE:
+                    self.or_gate()
+                elif self.symbol.id == self.scanner.NOR:
+                    self.nor()
+                elif self.symbol.id == self.scanner.DTYPE:
+                    self.dtype()
+                elif self.symbol.id == self.scanner.XOR:
+                    self.xor()
+                else:
+                    self.error()
+            else:
+                self.error()
+
+        #def switch(self):
+            #if self.symbol.type == self.scanner.KEYWORD:
+
+
+
         return True
