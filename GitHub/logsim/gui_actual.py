63304d08580908a479a6a248d573234df00e475d
Omar Zaman <omz20@cam.ac.uk>
2022-06-01 16:13:34 +0100

    GUI front end completed

--- GitHub/logsim/gui_actual.py
+++ GitHub/logsim/gui_actual.py
@@ -1,6 +1,10 @@
 import wx
 import wx.glcanvas as wxcanvas
 from OpenGL import GL, GLUT
+import numpy as np
+import wx.lib.scrolledpanel as scrolled
+
+
 
 class MyGLCanvas(wxcanvas.GLCanvas):
 
@@ -55,19 +59,38 @@ class MyGLCanvas(wxcanvas.GLCanvas):
         self.render_text(text, 10, 10)
 
         # Draw a sample signal trace
+
+        
         GL.glColor3f(0.0, 0.0, 1.0)  # signal trace is blue   Colour
 
         
         #Trace drawing
 
+
+        #Test trace data
+
+        data  = [(i//5) % 2  for i in range(20)]
+
         GL.glBegin(GL.GL_LINE_STRIP)
-        for i in range(10):
+        for i,val in enumerate(data):
+            y = 100 + val*25
+            x = (i * 20) + 10
+            x_next = (i * 20) + 30
+           
+            GL.glVertex2f(x, y)
+            GL.glVertex2f(x_next, y)
+        GL.glEnd()
+
+
+
+        GL.glBegin(GL.GL_LINE_STRIP)
+        for i in range(20):
             x = (i * 20) + 10
             x_next = (i * 20) + 30
             if i % 2 == 0:
-                y = 75
+                y = 150
             else:
-                y = 100
+                y = 175
             GL.glVertex2f(x, y)
             GL.glVertex2f(x_next, y)
         GL.glEnd()
@@ -127,13 +150,58 @@ class Gui(wx.Frame):
         self.scrollable = wx.ScrolledCanvas(self, wx.ID_ANY )
         self.scrollable.SetSizeHints(200, 200)
         self.scrollable.ShowScrollbars(wx.SHOW_SB_ALWAYS,wx.SHOW_SB_DEFAULT)
-        self.scrollable.SetScrollbars(20, 20, 15, 10)
+        self.scrollable.SetScrollbars(20, 20, 0, 0)
+        
+
+
+        #Switch Panel
+
+        self.panel = wx.ScrolledWindow(self,wx.ID_ANY,pos=(25,50),size = (300,175))
+        self.panel.SetBackgroundColour('White')
+
+        switch_x = 10
+        switch_y = 10
+        n = 12 #number of switches
+
+        self.text = wx.StaticText(self.panel, wx.ID_ANY, "Switches",pos = (switch_x,switch_y))
+        self.text = wx.StaticText(self.panel, wx.ID_ANY, "Current State",pos = (switch_x+70,switch_y))
+        self.text = wx.StaticText(self.panel, wx.ID_ANY, "Change State",pos = (switch_x+170,switch_y))
+
+
+        self.panel.ShowScrollbars(wx.SHOW_SB_ALWAYS,wx.SHOW_SB_DEFAULT)
+        self.panel.SetScrollbars(20, 10, 0, 4.5*n)
+
+        self.list_of_change_buttons = []
+        #self.change_button = wx.Button(self.panel, wx.ID_ANY, "Change",pos = (switch_x+170,50+ 40))
+
+        list_of_switch_names = [f'S{i+1}' for i in range(n)] #Switch name generation
+
+
+        #self.list_of_panels = [wx.Panel(self.panel,wx.ID_ANY,pos=(switch_x+70,50+40*i),size=(80,20)) for i in range(n)]
+
+        #self.colour_panel = wx.Panel(self.panel,wx.ID_ANY,pos=(switch_x+70,50),size=(80,20))
+        #self.colour_panel.SetBackgroundColour('Red')
+
+        list_of_switch_values = [0 for i in range(n)]
+        self.list_of_switch_text_values = []
+
+        for i in range(n):
+            self.list_of_change_buttons.append(wx.Button(self.panel, wx.ID_ANY, "Change",pos = (switch_x+170,50+ 40*i)))
+            self.text = wx.StaticText(self.panel, wx.ID_ANY, list_of_switch_names[i],pos = (switch_x+20,50+40*i))
+
+            #self.list_of_panels[i].SetBackgroundColour('Red')
+
+            self.list_of_switch_text_values.append(wx.StaticText(self.panel, wx.ID_ANY,str(list_of_switch_values[i]),pos = (switch_x+105,50+40*i)))
+
+
+
+        #self.panel.Bind(wx.EVT_PAINT, self.OnPaint)
+
 
 
 
 
         # Configure the widgets
-        self.text = wx.StaticText(self, wx.ID_ANY, "Some text",pos = (600,200))
 
         #buttons as part o widgets
         button1_x = 550
@@ -152,8 +220,8 @@ class Gui(wx.Frame):
         component_list = ['G1','G3','CLK','S1']
         Remove_list = ['A1','G2','D1']
 
-        self.run_spin_control = wx.SpinCtrl(self, wx.ID_ANY, value = '10', min=0, max=100,pos = (button1_x+120,button1_y))
-        self.continue_spin_control = wx.SpinCtrl(self, wx.ID_ANY, value = '10', min=0, max=100,pos = (button1_x+120,button1_y+50))
+        self.run_spin_control = wx.SpinCtrl(self, wx.ID_ANY, value = '10', min=0, max=20,pos = (button1_x+120,button1_y))
+        self.continue_spin_control = wx.SpinCtrl(self, wx.ID_ANY, value = '10', min=0, max=20,pos = (button1_x+120,button1_y+50))
         self.Add_Monitor_choices = wx.Choice(self, wx.ID_ANY,choices = component_list,pos = (button1_x+120,button1_y+100))
         self.Remove_Monitor_choices = wx.Choice(self, wx.ID_ANY,choices = Remove_list,pos = (button1_x+120,button1_y+150))
 
@@ -166,6 +234,12 @@ class Gui(wx.Frame):
         self.button_Remove_Monitor.Bind(wx.EVT_BUTTON, self.OnButton_Remove_Monitor)
         self.button_Quit.Bind(wx.EVT_BUTTON, self.OnButton_Quit)
 
+        #self.change_button.Bind(wx.EVT_BUTTON, self.OnButton_Change)
+
+        for i in range(n):
+            self.list_of_change_buttons[i].Bind(wx.EVT_BUTTON, self.getOnButton_Change(i))
+
+
 
 
 
@@ -177,32 +251,31 @@ class Gui(wx.Frame):
         # Configure sizers for layout
         main_sizer = wx.BoxSizer(wx.HORIZONTAL)
         side_sizer = wx.BoxSizer(wx.VERTICAL)
-       
+        new_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
         
 
         self.canvas = MyGLCanvas(self.scrollable, wx.ID_ANY, (25,250),  wx.Size(500,300))
 
-        #self.canvas = MyGLCanvas(self.scrollable, wx.ID_ANY, (25,25),  wx.Size(300,200))
+
+
         self.canvas.SetSizeHints(500, 500)
-
-
-
-       
-
-
         main_sizer.Add(self.scrollable, 1,  wx.EXPAND+wx.TOP, 5)
-        #main_sizer.Add(self.scrollable, 1,  wx.EXPAND+wx.TOP, 5)
-        controlwin = wx.ScrolledWindow(self, -1, wx.DefaultPosition, wx.DefaultSize, wx.SUNKEN_BORDER|wx.HSCROLL|wx.VSCROLL)
 
-        main_sizer.Add(controlwin, 1, wx.ALL, 5)
-        button_sizer2 = wx.BoxSizer(wx.VERTICAL)
-        controlwin.SetSizer(button_sizer2)
-        controlwin.SetScrollRate(10, 10)
-        controlwin.SetAutoLayout(True)
+        #new_sizer.Add(self.panel,1,  wx.EXPAND+wx.TOP, 5)
+        
+
+        
 
         self.SetSizeHints(200, 200)
         self.SetSizer(main_sizer)
+        #self.SetSizer(new_sizer)
+
+    def OnPaint(self,event=None):
+        dc = wx.PaintDC(self)
+        dc.Clear()
+        dc.SetPen(wx.Pen(wx.BLACK, 4))
+        dc.DrawLine(100, 100, 100, 200)
        
     def on_menu(self, event):
         """Handle the event when the user selects a menu item."""
@@ -247,6 +320,18 @@ class Gui(wx.Frame):
     def OnButton_Quit(self, event):
         """Handle the event when the user clicks button_Quit."""
         print ("Button Quit pressed")
+
+    def getOnButton_Change(self,i):
+
+        def OnButton_Change(event):
+            """Handle the event when the user clicks."""
+            #self.colour_panel.SetBackgroundColour('Green')
+            #self.list_of_panels[0].SetBackgroundColour('Green')
+            val = int(self.list_of_switch_text_values[i].GetLabel())
+            self.list_of_switch_text_values[i].SetLabel(f'{(val +1)%2}')
+            print (f"Button Change S{i+1} pressed")
+
+        return OnButton_Change
 
 app = wx.App()
 gui = Gui("Demo")
